5ab172f260ad9f00fe9e0f16a9f14ae034ab9b37
Rachel Thiim <rachel.thiim@gmail.com>
2021-04-22 22:12:04 -0500

    global state object created. Starting cleaning some unneeded props

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-
+import { StoreProvider } from "./utils/GlobalState";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
@@ -28,6 +28,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
+        <StoreProvider>
           <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
@@ -37,6 +38,7 @@ function App() {
             <Route exact path="/products/:id" component={Detail} />
             <Route component={NoMatch} />
           </Switch>
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
